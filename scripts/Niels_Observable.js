274d59155c80cb1d730f1576fbecac4a30c2eacc
Niels van Nimmen <niels.van.nimmen@gmail.com>
2016-10-14 11:02:56 +0200

    add observable frame

--- scripts/Niels_Observable.js
+++ scripts/Niels_Observable.js
@@ -1,1 +1,38 @@
-// Test
+var Observable = function() {
+    
+    var _self = this;
+    
+    _self.data;
+    
+    _self.subscribers = [];
+    
+    
+    _self.subscribe = function( newData ) {
+        
+        var data = _self.newData;
+        
+        if(newData === 'undefined')
+        {
+            return _self.data;
+        }
+        else
+        {
+            
+            
+            for(subscirber in _self.subscribers)
+            {
+                
+            }
+        }
+    }
+    
+    
+    _self.unsubscribe = function() {
+        
+    }
+    
+    
+    _self.publish = function() {
+        
+    }
+}
