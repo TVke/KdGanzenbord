cd5953d1485257c4f785caed5ce326aef38e6664
Niels van Nimmen <niels.van.nimmen@gmail.com>
2016-10-14 12:36:17 +0200

    add complete observable

--- scripts/Niels_Observable.js
+++ scripts/Niels_Observable.js
@@ -7,7 +7,7 @@ var Observable = function() {
     _self.subscribers = [];
     
     
-    _self.publish = function() {
+    _self.publish = function( newData ) {
         
         if(newData === 'undefined')
         {
@@ -17,9 +17,9 @@ var Observable = function() {
         {
             _self.data = newData;
             
-            for(subsriber in _self.subscribers)
+            for(var subscriberKey in _self.subscribers)
             {
-                _self.subscribers.push(newData);
+                _self.subscribers[subscriberKey](newData);
             }
         }
     }
@@ -27,12 +27,18 @@ var Observable = function() {
     
     _self.subscribe = function( callback ) {
         
-        _self.subscribers
+        _self.subscribers.push(callback);
     }
     
     
     _self.unsubscribe = function( callback ) {
         
-        
+        for(var unsubscriberKey in _self.subscribers)
+        {
+            if(_self.subscribers[unsubscriberKey] === callback)
+            {
+                delete _self.subscribers[unsubscriberKey];
+            }
+        }
     }
 };
