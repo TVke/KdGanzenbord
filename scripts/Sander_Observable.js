a6dfdbd1c973364d154e203db07bb24bcc40190a
Sander Borret <sander.borret@student.kdg.be>
2016-10-14 16:15:03 +0200

    add custom observable

---
+++ scripts/Sander_Observable.js
@@ -0,0 +1,37 @@
+function Observable() {
+	var _self = this;
+	_self.data;
+	_self.subscribers = [];
+
+	_self.methods = {
+		subscribe : function(callback) {
+			if(_self.subscribers.indexOf(callback) === -1) {
+				_self.subscribers.push(callback);
+			}
+		},
+
+		unsubscribe : function(callback) {
+			if(_self.subscribers.indexOf(callback) > -1) {
+				for (var subscribersIndex; subscribersIndex < _self.subscribers.length; ++subscribersIndex) {
+		            if (_self.subscribers[i] === callback) {
+		                _self.subscribers.splice(subscribersIndex, 1);
+		                return;
+	            	}
+				}
+			}
+		},
+
+		publish : function(data) {
+			if(typeof data !== "undefined") {
+				_self.data = data;
+				for(var subscribersIndex = 0; subscribersIndex < _self.subscribers.length; ++subscribersIndex) {
+		            _self.subscribers[subscribersIndex](data);
+		        }
+			}
+			else {
+				return _self.data;
+			}
+		}
+	}
+	return _self.methods;
+}
